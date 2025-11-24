6b0903c9f04dfb6fd74ccf4ce43214f2aca3c385
OluchukwuOkafor <okaforoluchukwu65@gmail.com>
2025-11-24 20:31:40 +0100

    Implement token authentication and permissions in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
 
 # Existing ListAPIView
 class BookList(ListAPIView):
@@ -12,3 +14,8 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -28,3 +28,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    ...
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
