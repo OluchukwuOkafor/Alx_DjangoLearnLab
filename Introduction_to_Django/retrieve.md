e893e3e1e7aa462e74572a40d9c669daefa1f2fa
OluchukwuOkafor <okaforoluchukwu65@gmail.com>
2025-12-09 12:49:36 +0100

    Fix BookSerializer, BookList view, and API URLs for ALX checks

--- Introduction_to_Django/retrieve.md
+++
@@ -1,5 +0,0 @@
-# Retrieve Operation
-
-```python
-from bookshelf.models import Book
-Book.objects.all()
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -19,3 +19,7 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
