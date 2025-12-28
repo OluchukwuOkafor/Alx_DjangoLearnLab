c334637631c3e0e7064b0b797ee87c34c3ff64c9
OluchukwuOkafor <okaforoluchukwu65@gmail.com>
2025-12-28 23:27:15 +0100

    Add comment functionality to blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
+
 class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
@@ -51,3 +52,33 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
