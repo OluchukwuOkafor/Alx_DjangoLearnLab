24c274014bc4a513aebc5fe2efde9da662c236f7
OluchukwuOkafor <okaforoluchukwu65@gmail.com>
2026-01-01 21:46:39 +0100

    Fix query_samples.py with correct ORM queries

---
+++ relationship_app/query_samples.py
@@ -0,0 +1,22 @@
+from .models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        return Book.objects.filter(author=author)
+    except Author.DoesNotExist:
+        return Book.objects.none()
+
+def books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return Book.objects.none()
+
+def librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
